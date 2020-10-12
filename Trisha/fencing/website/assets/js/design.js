6d9e88254b15ac9377b85a41bacfa0c262941d7a
Harsh97 <harsh97.au@gmail.com>
2020-10-12 23:51:01 +1030

    integrate api of design and display data

--- Trisha/fencing/website/assets/js/design.js
+++ Trisha/fencing/website/assets/js/design.js
@@ -1,3 +1,4 @@
+
 /**
 * Template Name: MyPortfolio - v2.1.0
 * Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
@@ -69,3 +70,208 @@
   });
 
 })(jQuery);
+
+//Start
+var design;
+function category(value)
+{
+  design = value;
+  var settings = 
+  {
+    "url": "http://localhost:3000/admin",
+    "method": "POST",
+    "timeout": 0,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "data": JSON.stringify({"design":value}),
+  };
+  
+  $.ajax(settings).done(function (response)
+   {
+      // console.log(response[0]['content']);
+      // console.log(response[0]['title']);
+      //console.log(response[0]['image']);
+      
+      var title = response[0]['title'];
+      var content = response[0]['content'];
+      //var image = response[0]['image'];
+      document.getElementById("title").innerHTML = title;
+      //document.getElementById("img").innerHTML = image;
+      document.getElementById("content").innerHTML = content;
+  });
+}
+
+//Update Designs
+function change() 
+{
+  var designChange = window.design;
+  var title = document.getElementById("titleChange").value; 
+  var content = document.getElementById("contentChange").value;  
+
+  if(designChange == "" || title == "" || content == "")
+  {
+    return alert("Please Enter the values in the fields!!");
+  }
+
+  var settings = {
+    "url": "http://localhost:3000/update",
+    "method": "POST",
+    "timeout": 0,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "data": JSON.stringify({"design":designChange,"name":title,"content":content}),
+  };
+  
+  $.ajax(settings).done(function (response) {
+    // console.log(response);
+    alert(response);
+    window.location.reload();
+    return;
+  });
+  
+}
+
+//Upload Image
+// function img()
+// {
+//   var form = $('#fileUploadForm')[0];
+
+// 		// Create an FormData object
+//   var data = new FormData(form);
+
+//   var designChange = window.design;
+//   var img = document.getElementById("changeImg").value;
+//   var settings = {
+//         type: "POST",
+//         enctype: 'multipart/form-data',
+//         processData: false,  // Important!
+//         contentType: false,
+//         cache: false,
+//         url: "http://localhost:3000/fileUpload",
+//         data
+//   };
+  
+//   $.ajax(settings).done(function (response) {
+//     // console.log(response);
+//     return alert(response);
+//   });
+// }
+
+//Fetch design data
+function designsRetaining() 
+{
+  var settings = {
+    "url": "http://localhost:3000/fetchdesign",
+    "method": "POST",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) { 
+    var titleRetaining = response[0]['title'];
+    var contentRetaining = response[0]['content'];
+
+    document.getElementById("titleRetaining").innerHTML = titleRetaining;
+    document.getElementById("subtitleRetaining").innerHTML = titleRetaining;
+    document.getElementById("contentRetaining").innerHTML = contentRetaining;
+  });
+  
+}
+
+function  designNeighbour() 
+{
+  var settings = {
+    "url": "http://localhost:3000/fetchdesign",
+    "method": "POST",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    var titleNeighbour  = response[1]['title'];
+    var contentNeighbour = response[1]['content'];
+
+    document.getElementById("titleNeighbour").innerHTML = titleNeighbour;
+    document.getElementById("subtitleNeighbour").innerHTML = titleNeighbour;
+    document.getElementById("contentNeighbour").innerHTML = contentNeighbour;
+  });
+}
+
+function designPost() 
+{
+   var settings = {
+    "url": "http://localhost:3000/fetchdesign",
+    "method": "POST",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    var titlePost = response[2]['title'];
+    var contentPost = response[2]['content'];
+
+    document.getElementById("titlePost").innerHTML = titlePost;
+    document.getElementById("subtitlePost").innerHTML = titlePost;
+    document.getElementById("contentPost").innerHTML = contentPost;
+  });
+  
+}
+
+function designWallFence() 
+{
+   var settings = {
+    "url": "http://localhost:3000/fetchdesign",
+    "method": "POST",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    var titleWallFence  = response[3]['title'];
+    var contentWallFence = response[3]['content'];
+
+    document.getElementById("titleWallFence").innerHTML = titleWallFence;
+    document.getElementById("subtitleWallFence").innerHTML = titleWallFence;
+    document.getElementById("contentWallFence").innerHTML = contentWallFence;
+
+  });
+  
+}
+
+function designSalt() 
+{
+   var settings = {
+    "url": "http://localhost:3000/fetchdesign",
+    "method": "POST",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    var titleSalt = response[4]['title'];
+    var contentSalt = response[4]['content'];
+
+    document.getElementById("titleSalt").innerHTML = titleSalt;
+    document.getElementById("subtitleSalt").innerHTML = titleSalt;
+    document.getElementById("contentSalt").innerHTML = contentSalt;
+
+  });
+  
+}
+
+function designTubular() 
+{
+   var settings = {
+    "url": "http://localhost:3000/fetchdesign",
+    "method": "POST",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    var titleTubular = response[5]['title'];
+    var contentTubular= response[5]['content'];
+
+    document.getElementById("titleTubular").innerHTML = titleTubular;
+    document.getElementById("subtitleTubular").innerHTML = titleTubular;
+    document.getElementById("contentTubular").innerHTML = contentTubular;
+
+  });
+  
+}
